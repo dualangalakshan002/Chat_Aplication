76bc8f2c303f4b54fab8813fcff2ca037f246e03
Dulanga Lakshan <140315525+dualangalakshan002@users.noreply.github.com>
2025-09-20 10:51:37 +0530

    socket don

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,11 +7,13 @@ import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js"
+import { app, server } from "./lib/socket.js";
 
 
 
 dotenv.config()
-const app = express();
+app
+
 
 const PORT = process.env.PORT || 5002;
 
@@ -27,7 +29,7 @@ app.use(cors({
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes)
 
-app.listen (PORT, () =>{
+server.listen (PORT, () =>{
     console.log("Server is running on PORT:"+PORT);
     connectDB()
 
