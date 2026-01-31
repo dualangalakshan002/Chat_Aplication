f424e048a90a6138a8350fb276aca12669ec7ccf
Dulanga Lakshan <140315525+dualangalakshan002@users.noreply.github.com>
2026-01-31 21:07:39 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,6 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
 import cors from "cors";
 
-import path from "path";
-
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -18,7 +16,6 @@ app
 
 
 const PORT = process.env.PORT || 5002;
-const __dirname = path.resolve();
 
 app.use(express.json({limit:"10mb"}));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -31,14 +28,6 @@ app.use(cors({
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes)
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-    });
-}
 
 server.listen (PORT, () =>{
     console.log("Server is running on PORT:"+PORT);
